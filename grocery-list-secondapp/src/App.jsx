9554c18fb99ac50f74ef08e27939bafd361c63c0
Michael Valdez <mikeyvaldez92@yahoo.com>
2024-10-15 16:31:35 -0700

    two way binding

---
+++ grocery-list-secondapp/src/App.jsx
@@ -0,0 +1,46 @@
+import './App.css'
+import groceryCartImg from './assets/grocery-cart.png'
+import { useState } from 'react'
+
+function App() {
+
+  const [inputValue, setInputValue] = useState('')
+
+  const handleChangeInputValue = (e) => {
+    setInputValue(e.target.value)
+  }
+  
+  return (
+    <main className='App'>
+      <div>
+      <div>
+        <h4 className='success'>You are Done!</h4>
+        <div className='header'>
+          <h1>Shopping List</h1>
+          <img src={groceryCartImg} alt='' />
+          <input
+            type='text'
+            placeholder='Add an Item'
+            className='item-input'
+            onChange={handleChangeInputValue}
+            value={inputValue}
+            />
+        </div>
+      </div>
+      <ul>
+        <li>
+          <div className='container'>
+            <input type='checkbox' />
+            <p>Carrots</p>
+          </div>
+          <div>
+            <button className='remove-button' >X</button>
+          </div>
+        </li>
+      </ul>
+      </div>              
+    </main>
+  );
+}
+
+export default App
