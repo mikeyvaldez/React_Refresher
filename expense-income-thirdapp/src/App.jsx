87cb0d935bfbcc7c60fd1a1ed6df3883b01658f4
Michael Valdez <mikeyvaldez92@yahoo.com>
2024-10-17 20:53:12 -0700

    edit app

--- expense-income-thirdapp/src/App.jsx
+++ expense-income-thirdapp/src/App.jsx
@@ -1,11 +1,13 @@
+/* eslint-disable no-unreachable */
 import { useState } from 'react'
 import './App.css'
 
 function App() { 
+  const [statements, setStatements] = useState([])
   const [input, setInput] = useState({
     statement: "",
     amount: "",
-    statementType: ""
+    statementType: "income"
   })
 
   const [showError, setShowError] = useState({
@@ -20,69 +22,102 @@ function App() {
     })
   }
 
-  const handleAddNewStatement = () => {
-    const {statement, amount} = input
+  const handleAddNewStatement = () => {    
+    const { statement, amount, statementType } = input;
 
-    if(!statement){
+    if (!statement) {
       return setShowError({
         statement: true,
-        amount: false
-      })
-    } else if(!amount){
+        amount: false,
+      });
+    } else if (!amount) {
       return setShowError({
         statement: false,
-        amount: true
-      })
-    } else{
-      // ADD LOGIC TO ADD STATEMENT
-      return setShowError({
+        amount: true,
+      });
+    } else {
+      setShowError({
         statement: false,
-        amount: false
-      })
+        amount: false,
+      });
+      // ADD LOGIC TO ADD STATMENT
+      setStatements([
+        ...statements,
+        {
+          name: statement,
+          amount: parseFloat(amount).toFixed(2),
+          type: statementType,
+          date: new Date().toDateString(),
+        },
+      ]);
+      setInput({
+        statement: "",
+        amount: "",
+        statementType: "income",
+      });
     }
-  }
+  };   
+
 
   return (
     <main>
       <div>
-        <h1 className='total-text'>0</h1>
+        <h1 className="total-text">0</h1>
         <div className="input-container">
           <input
-            type='text'
-            placeholder='Income or expense'
+            type="text"
+            placeholder="Income or expense"
             onChange={handleUpdateInput}
             value={input.statement}
-            name='statement'
-            style={showError.statement ? {borderColor: 'rgb(206, 76, 76'} : null}
+            name="statement"
+            style={
+              showError.statement ? { borderColor: "rgb(206, 76, 76)" } : null
+            }
           />
           <input
-            type='number'
-            placeholder='$5000'
+            type="number"
+            placeholder="$5000"
             onChange={handleUpdateInput}
             value={input.amount}
-            name='amount'
-            style={showError.amount ? {borderColor: 'rgb(206, 76, 76'} : null}
+            name="amount"
+            style={
+              showError.amount ? { borderColor: "rgb(206, 76, 76)" } : null
+            }
           />
           <select
             onChange={handleUpdateInput}
             value={input.statementType}
-            name='statementType'
-          >            
-            <option value='income'>Income</option>
-            <option value='expense'>Expense</option>
+            name="statementType"
+          >
+            <option value="income">Income</option>
+            <option value="expense">Expense</option>
           </select>
           <button onClick={handleAddNewStatement}>+</button>
         </div>
-        <div className='card'>
-          <div className='card-info'>
-            <h4>Salary</h4>
-            <p>July 27th, 2024</p>
-          </div>
-          <p className='amount-text success'>+$5000</p>
+        <div>
+          <div className="card">
+            <div className="card-info">
+              <h4>Salary</h4>
+              <p>July 27th, 2024</p>
+              {statements.map(({ name, type, amount, date }) => (
+                <><div className="card">
+                  <div className="card-info">
+                    <h4>{name}</h4>
+                    <p>{date}</p>
+                  </div>
+                  <p
+                    className={`amount-text ${type === "income" ? "success" : "danger"}`}
+                  >
+                    {type === "income" ? "+" : "-"}${amount}
+                  </p>
+                </div><p className="amount-text success">+$5000</p></>          
+              ))}
+            </div>
+          </div>          
         </div>
       </div>
     </main>
   );
 }
 
-export default App
+export default App;
