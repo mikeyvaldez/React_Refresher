e67a7a768021db21df9ba76957adc98cdd003df5
Michael Valdez <mikeyvaldez92@yahoo.com>
2024-10-25 16:08:57 -0700

    passing props to components

---
+++ third-recipe/src/components/Header.jsx
@@ -0,0 +1,30 @@
+function Header() {
+  return (
+    <header className="main_header">
+      <div className="text-container">
+        <h1 className="header-title">
+          Search for <span>Delicious</span> Food
+        </h1>
+        <p className="header-description">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          Reprehenderit necessitatibus magni laudantium temporibus obcaecati
+          odio sint sunt animi repellendus, aliquid, eligendi nobis amet aperiam
+          deserunt nesciunt quia ad beatae accusamus?
+        </p>
+        <div className="header-input-container">
+          <input type="text" placeholder="Find a recipe" />
+          <button>Search</button>
+        </div>
+      </div>
+      <div>
+        <img
+          src="https://www.bakerella.com/site/wp-content/uploads/2015/01/cinnamonrolls-720x540.jpg"
+          alt=""
+          className="main_img"
+        />
+      </div>
+    </header>
+  );
+}
+
+export default Header;
