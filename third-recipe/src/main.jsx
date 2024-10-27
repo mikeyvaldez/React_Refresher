ba2c74420a51b764c6c0ffbe011f3bf3ffef7380
Michael Valdez <mikeyvaldez92@yahoo.com>
2024-10-27 12:53:12 -0700

    created about page and implemented the component on main file

--- third-recipe/src/main.jsx
+++ third-recipe/src/main.jsx
@@ -7,9 +7,13 @@ import {
 } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
+import AboutPage from "./pages/AboutPage.jsx";
 
 const router = createBrowserRouter(
-  createRoutesFromElements(<Route path="/" element={<App />} />)
+  createRoutesFromElements(<>
+  <Route path="/" element={<App />} />
+  <Route path="/about" element={<AboutPage />} />
+  </>)
 );
 
 createRoot(document.getElementById("root")).render(<RouterProvider router={router} />);
