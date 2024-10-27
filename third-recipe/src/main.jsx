bc3da422f22925d88c3197d96dc561e854528b3d
Michael Valdez <mikeyvaldez92@yahoo.com>
2024-10-27 12:47:16 -0700

    edit main file and added react router components

--- third-recipe/src/main.jsx
+++ third-recipe/src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import {
+  RouterProvider,
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter(
+  createRoutesFromElements(<Route path="/" element={<App />} />)
+);
+
+createRoot(document.getElementById("root")).render(<RouterProvider router={router} />);
