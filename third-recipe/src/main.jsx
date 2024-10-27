4a113a0e5a395c1532dcab1dd579511847251120
Michael Valdez <mikeyvaldez92@yahoo.com>
2024-10-27 14:45:37 -0700

    edit to app about main and homepage

--- third-recipe/src/main.jsx
+++ third-recipe/src/main.jsx
@@ -8,12 +8,15 @@ import {
 import "./index.css";
 import App from "./App.jsx";
 import AboutPage from "./pages/AboutPage.jsx";
+import HomePage from "./pages/HomePage.jsx";
 
 const router = createBrowserRouter(
-  createRoutesFromElements(<>
-  <Route path="/" element={<App />} />
-  <Route path="/about" element={<AboutPage />} />
-  </>)
+  createRoutesFromElements(  
+  <Route path="/" element={<App />}>  {/**parent route */}
+    <Route path="/" element={<HomePage />} />
+    <Route path="/about" element={<AboutPage />} />
+  </Route>
+  )
 );
 
 createRoot(document.getElementById("root")).render(<RouterProvider router={router} />);
--- third-recipe/src/pages/HomePage.jsx
+++ third-recipe/src/pages/HomePage.jsx
@@ -1,14 +1,11 @@
-import Header from "../components/Header"
-import CardList from "../components/CardList"
-
+import CardList from "../components/CardList";
+import Header from "../components/Header";
 
 export default function HomePage() {
   return (
-    <div className="App">
-      <main className="main_container">
-        <Header />
-        <CardList />
-      </main>
-    </div>
+    <>
+      <Header />
+      <CardList />
+    </>
   );
 }
