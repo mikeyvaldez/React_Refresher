7b1a9cbb5b9b9fbabea331a3139d6f32ffdcf6fa
Michael Valdez <mikeyvaldez92@yahoo.com>
2024-10-29 16:53:51 -0700

    the children prop

--- third-recipe/src/components/RecipeHeader.jsx
+++ third-recipe/src/components/RecipeHeader.jsx
@@ -1,6 +1,10 @@
 /* eslint-disable react/prop-types */
 
 import RecipeNutritionalFact from "./RecipeNutritionalFact";
+import { FaFireAlt } from "react-icons/fa";
+import { CiWheat } from "react-icons/ci";
+import { BiCheese, BiCake } from "react-icons/bi";
+import { IoFishOutline } from "react-icons/io5";
 
 export default function RecipeHeader({ nutritionalFacts }) {
   const nutritionalFactsArray = [
@@ -8,26 +12,31 @@ export default function RecipeHeader({ nutritionalFacts }) {
       id: 1,
       amount: nutritionalFacts.calories,
       category: "calories",
+      Icon: FaFireAlt
     },
     {
       id: 2,
       amount: nutritionalFacts.carbohydrates,
       category: "carbs",
+      Icon: CiWheat
     },
     {
       id: 3,
       amount: nutritionalFacts.fat,
       category: "fats",
+      Icon: BiCheese
     },
     {
       id: 4,
       amount: nutritionalFacts.protein,
       category: "proteins",
+      Icon: IoFishOutline
     },
     {
       id: 5,
       amount: nutritionalFacts.sugar,
       category: "sugars",
+      Icon: BiCake
     },
   ];
 
@@ -35,8 +44,10 @@ export default function RecipeHeader({ nutritionalFacts }) {
     <div className="recipe-header">
       <h1>Chai-Spiced Cheesecake Muffins</h1>
       <div className="nutritional-facts-container">
-        {nutritionalFactsArray.map((fact) => (
-          <RecipeNutritionalFact fact={fact} key={fact.id} />
+        {nutritionalFactsArray.map(({Icon, id, amount, category}) => (
+          <RecipeNutritionalFact fact={{amount, category}} key={id}>
+            <Icon />
+          </RecipeNutritionalFact>
         ))}
       </div>
     </div>
--- third-recipe/src/components/RecipeNutritionalFact.jsx
+++ third-recipe/src/components/RecipeNutritionalFact.jsx
@@ -1,8 +1,10 @@
 /* eslint-disable react/prop-types */
-export default function RecipeNutritionalFact({ fact }) {
+export default function RecipeNutritionalFact({ fact, children }) {
+    
+
   return (
     <div className="recipe-fact-container">
-      <img src="" alt="" />
+      {children}      
       <h3>{fact.amount}</h3>
       <p>{fact.category}</p>
     </div>
