490d07ba12683289736243f3dc7d3086714e9361
Michael Valdez <mikeyvaldez92@yahoo.com>
2024-10-30 14:07:04 -0700

    handling error state with an error message

--- third-recipe/src/pages/RecipePage.jsx
+++ third-recipe/src/pages/RecipePage.jsx
@@ -4,6 +4,7 @@ import { useEffect } from "react";
 import RecipeHeader from "../components/RecipeHeader";
 import Loading from "../components/Loading";
 import RecipeInfo from "../components/RecipeInfo";
+import Error from "../components/Error";
 
 export default function RecipePage() {
   const { id } = useParams();
@@ -16,6 +17,8 @@ export default function RecipePage() {
   
   if (loading) return <Loading />;
   if (error) return <h1>{error}</h1>;
+  if (data?.errors) return <Error explanation="Recipe not found" />
+  console.log(data);
 
   return (
     <div>
