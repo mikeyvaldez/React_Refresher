d0a3cb76cae1f5ceccb0b339e8a27daee569c05b
Michael Valdez <mikeyvaldez92@yahoo.com>
2024-11-04 12:05:16 -0800

    state with typescript

--- fourth-typescript/react-with-ts/src/App.tsx
+++ fourth-typescript/react-with-ts/src/App.tsx
@@ -1,11 +1,18 @@
 import "./App.css";
+import Modal from "./components/Modal";
 
 function App() {
+  const [showModal, setShowModal] = useState(false);
+
+  const handleOpen = () => setShowModal(true)
+  const handleClose = () => setShowModal(false)
+
   return (
     <div className="App">
       <h1>My Birthday Gifts</h1>
+      { showModal && <Modal /> }
       <div className="cards">
-        <button>Add a Gift</button>
+        <button onClick={handleOpen} >Add a Gift</button>
       </div>
       <div className="cards-gift">
         {[].map((gift) => (
